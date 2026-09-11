9fd50afe2e6d9e85a3d70dd2d4b0ae9a820285d5
Viktor Rudebeck <viktor@rudebeck.nu>
2026-09-11 11:36:04 +0200

    Change to make `Names` a sealed abstract class

--- modules/generic/src/main/scala-3/vulcan/internal/Names.scala
+++ modules/generic/src/main/scala-3/vulcan/internal/Names.scala
@@ -5,16 +5,39 @@
  */
 
 package vulcan.internal
+
 import vulcan.internal.NamesMacro
 
-case class Names[A](
-  typeName: String,
-  namespace: String,
-  doc: Option[String],
-  aliasOf: Option[String]
-)
+sealed abstract class Names[A] {
+  def typeName: String
+
+  def namespace: String
+
+  def doc: Option[String]
+
+  def aliasOf: Option[String]
+}
 
 object Names {
+  private final case class NamesImpl[A](
+    override val typeName: String,
+    override val namespace: String,
+    override val doc: Option[String],
+    override val aliasOf: Option[String]
+  ) extends Names[A]
+
+  private[internal] def apply[A](
+    typeName: String,
+    namespace: String,
+    doc: Option[String],
+    aliasOf: Option[String]
+  ): Names[A] =
+    NamesImpl(
+      typeName = typeName,
+      namespace = namespace,
+      doc = doc,
+      aliasOf = aliasOf
+    )
 
   inline given [A]: Names[A] = NamesMacro.names[A]
 }
